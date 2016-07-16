63bee932534e78960e9f416a7eee6693f4d7b3d8
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-16 16:58:01 +0300

    Transpile server on build:prod

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 var assetsPath = path.join(__dirname, '..', 'public', 'assets');
 var publicPath = '/assets/';
 
-module.exports = {
+module.exports = [{
   // The configuration for the client
   name: 'browser',
   // SourceMap without column-mappings
@@ -33,5 +33,43 @@ module.exports = {
     root: [path.join(__dirname, '..', 'app')],
     extensions: ['', '.js', '.jsx']
   },
-};
+}, {
+  // The configuration for the server-side rendering
+  name: 'server-side rendering',
+  context: path.join(__dirname, '..', 'app'),
+  entry: {
+    server: './server.js'
+  },
+  target: 'node',
+  output: {
+    // The output directory as absolute path
+    path: assetsPath,
+    // The filename of the entry chunk as relative path inside the output.path directory
+    filename: 'server.js',
+    // The output path from the view of the Javascript
+    publicPath: '/assets/',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    loaders:[{
+      test: /\.js$|\.jsx$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      },
+      include: path.join(__dirname, '..', 'app'),
+      exclude: path.join(__dirname, '..', 'node_modules')
+    }]
+  },
+  resolve: {
+    root: [path.join(__dirname, '..', 'app')],
+    extensions: ['', '.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __DEVCLIENT__: false,
+      __DEVSERVER__: true
+    })
+  ]
+}];
 
