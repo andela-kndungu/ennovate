00f3689cb494325e7d7c5c9b065cccec5cfb82f9
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-16 18:29:57 +0300

    Buggy but a start

--- app/server.js
+++ app/server.js
@@ -3,7 +3,10 @@ import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 import createRoutes from './routes.jsx';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
 export default function render(req, res) {
+  // Needed for onTouchTap
+  // // http://stackoverflow.com/a/34015469/988941
   const routes = createRoutes();
 
   match({ routes, location: req.url }, (error, redirect, props) => {
@@ -12,6 +15,7 @@ export default function render(req, res) {
     } else if (redirect) {
       res.redirect(302, redirect.pathname + redirect.search);
     } else if (props) {
+      injectTapEventPlugin();
       const componentHTML = renderToString(
         <RouterContext {...props} />
       );
