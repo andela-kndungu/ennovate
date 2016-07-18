780fcd5f9fd339bd68f257f0a5631c473f2666cf
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-18 16:06:52 +0300

    It works! 😀😀😀
    Now we can start

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -5,71 +5,22 @@ var assetsPath = path.join(__dirname, '..', 'public', 'assets');
 var publicPath = '/assets/';
 
 module.exports = [{
-  // The configuration for the client
-  name: 'browser',
-  // SourceMap without column-mappings
-  devtool: 'cheap-module-source-map',
-  context: path.join(__dirname, '..', 'app'),
+  name: 'Non-server-side',
+  entry: path.join(__dirname, '../app/index.js'),
+  devtool: 'source-map',
   output: {
-    // The output directory as absolute path
     path: assetsPath,
-    // The filename of the entry chunk as relative path inside the output.path directory
-    filename: 'bundle.js',
-    // The output path from the view of the Javascript
-    publicPath: publicPath
+    filename: 'bundle.js'
   },
   module: {
     loaders: [{
-      test: /\.js$|\.jsx$/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react'],
-      },
-      include: path.join(__dirname, '..', 'app'),
-      exclude: path.join(__dirname, '..', 'node_modules')
-    }]
-  },
-  resolve: {
-    root: [path.join(__dirname, '..', 'app')],
-    extensions: ['', '.js', '.jsx']
-  },
-}, {
-  // The configuration for the server-side rendering
-  name: 'server-side rendering',
-  context: path.join(__dirname, '..', 'app'),
-  entry: {
-    server: './server.js'
-  },
-  target: 'node',
-  output: {
-    // The output directory as absolute path
-    path: assetsPath,
-    // The filename of the entry chunk as relative path inside the output.path directory
-    filename: 'server.js',
-    // The output path from the view of the Javascript
-    publicPath: '/assets/',
-    libraryTarget: 'commonjs2'
-  },
-  module: {
-    loaders:[{
-      test: /\.js$|\.jsx$/,
+      test: /\.jsx?$/,
       loader: 'babel-loader',
       query: {
         presets: ['es2015', 'react']
       },
-      include: path.join(__dirname, '..', 'app'),
       exclude: path.join(__dirname, '..', 'node_modules')
     }]
-  },
-  resolve: {
-    root: [path.join(__dirname, '..', 'app')],
-    extensions: ['', '.js', '.jsx'],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __DEVCLIENT__: false,
-      __DEVSERVER__: true
-    })
-  ]
+  }
 }];
 
