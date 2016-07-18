53307c6825fa5a171b379bb2c18dfb1a110c27d1
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-18 17:58:15 +0300

    Add AppBar

---
+++ app/components/AppBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const MyAppBar = () => {
+  return (
+    <AppBar
+      title="ennovate"
+    />
+  );
+};
+
+export default MyAppBar;
+
