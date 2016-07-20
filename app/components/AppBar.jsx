f74efc9e7fdd53abcf38551a487e7408994a3f77
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-20 12:46:39 +0300

    Add login button

--- app/components/AppBar.jsx
+++ app/components/AppBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 
 const MyAppBar = () => {
   return (
     <AppBar
       title="ennovate"
+      iconElementRight={<FlatButton label="Login" />}
     />
   );
 };
