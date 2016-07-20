d04292d8c8b15396f14a06145e9cbb61f805869a
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-20 14:56:16 +0300

    Login/Signup dialogue taking shape

--- app/components/AppBar.jsx
+++ app/components/AppBar.jsx
@@ -1,15 +1,63 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import ActionAndroid from 'material-ui/svg-icons/action/input';
+import Dialog from 'material-ui/Dialog';
+import Tabs from './login/Tabs.jsx';
 
-const MyAppBar = () => {
-  return (
-    <AppBar
-      title="ennovate"
-      iconElementRight={<FlatButton label="Login" />}
-    />
-  );
-};
+class MyAppBar extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { open: false };
+
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleOpen() {
+    this.setState({ open: true });
+  }
+
+  handleClose() {
+    this.setState({ open: false });
+  }
+
+  handleClick() {
+    alert('hello');
+  }
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+    return (
+      <AppBar
+        title="ennovate"
+        iconElementRight={
+          <FlatButton
+            icon={<ActionAndroid />}
+            label="LOGIN"
+            onTouchTap={this.handleOpen}
+          />
+          }
+      >
+        <Dialog
+          title={<Tabs />}
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+        </Dialog>
+      </AppBar>
+    );
+  }
+}
 
 export default MyAppBar;
 
