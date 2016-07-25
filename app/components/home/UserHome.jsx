3652b3455469abe1e8738c58ac599c51b201b4c9
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-25 09:32:50 +0300

    Add popover, authenticate from local token

--- app/components/home/UserHome.jsx
+++ app/components/home/UserHome.jsx
@@ -1,19 +1,66 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
-const MyAppBar = (props) => {
-  return (
-    <AppBar
-      title="ennovate"
-      iconElementRight={
-        <FlatButton
-          label={props.username}
+class MyAppBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { open: false };
+
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+  }
+
+  handleTouchTap(event) {
+    // This prevents ghost click.
+    event.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="ennovate"
+          iconElementRight={
+            <FlatButton
+              label={this.props.username}
+              onTouchTap={this.handleTouchTap}
+            />
+            }
         />
-        }
-    />
-  );
-};
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+          targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+          onRequestClose={this.handleRequestClose}
+        >
+          <Menu>
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Help &amp; feedback" />
+            <MenuItem primaryText="Settings" />
+            <MenuItem primaryText="Sign out" />
+          </Menu>
+        </Popover>
+      </div>
+    );
+  }
+}
 
 MyAppBar.propTypes = React.PropTypes.string.isRequired;
 
--- app/index.js
+++ app/index.js
@@ -7,7 +7,24 @@ import Main from './components/Main.jsx';
 import store from './redux/store';
 
 injectTapEventPlugin();
+const loginHelper = () => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    let base64Url = '';
+    base64Url = token.split('.')[1];
+    const username = JSON.parse(window.atob(base64Url))._doc.username;
+    return store.dispatch({
+      type: 'LOG_IN_USER_SUCCESS',
+      payload: {
+        token,
+        username
+      }
+    });
+  }
+  return null;
+};
 
+console.log(loginHelper());
 render(
   <Provider store={store}>
     <Main />
