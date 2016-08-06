6bd6841991bf7e99abd0f64903bca9b37830de7a
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-08-06 23:39:37 +0300

    Change styling of document card

--- app/components/cards/Test.jsx
+++ app/components/cards/Test.jsx
@@ -2,26 +2,26 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
-const CardExampleWithAvatar = (props) => (
-  <Card style={{ width: '300px', margin: '20px', float: 'left' }}>
+const CardExampleWithAvatar = (props) => {
+  return (<Card style={{ width: '300px', margin: '20px', float: 'left' }}>
     <CardHeader
       style={{ fontSize: '15px' }}
       title="kinuthia"
       subtitle={<div style={{ fontSize: '11px' }}><div>{props.date}</div><div>{props.date}</div></div>}
       avatar="http://www.lovemarks.com/wp-content/uploads/profile-avatars/default-avatar-tech-guy.png"
+      actAsExpander
     />
-    <CardTitle title={props.title} />
-    <CardText>
+    <CardTitle title={props.title} actAsExpander />
+    <CardText expandable>
       <div>
         {props.content}
       </div>
     </CardText>
-    <CardActions>
-      <FlatButton label="View" />
-      <FlatButton label="Action2" />
+    <CardActions style={{ textAlign: 'right' }}>
+      <FlatButton label="Edit" secondary={true} />
     </CardActions>
   </Card>
-
-);
+  );
+}
 
 export default CardExampleWithAvatar;
