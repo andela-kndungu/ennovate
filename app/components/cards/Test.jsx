39bba2af80b6490c1347c3324cc725171ec7a3a1
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-08-06 23:49:48 +0300

    Display time and date on document card

--- app/components/cards/Test.jsx
+++ app/components/cards/Test.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import moment from 'moment';
 
 const CardExampleWithAvatar = (props) => {
   return (<Card style={{ width: '300px', margin: '20px', float: 'left' }}>
     <CardHeader
       style={{ fontSize: '15px' }}
       title="kinuthia"
-      subtitle={<div style={{ fontSize: '11px' }}><div>{props.date}</div><div>{props.date}</div></div>}
+      subtitle={
+        <div style={{ fontSize: '11px' }}>
+          <div>{moment(props.date).format('Do MMMM YYYY')}</div>
+          <div>{moment(props.date).format('h:mm:ss a')}</div>
+      </div>}
       avatar="http://www.lovemarks.com/wp-content/uploads/profile-avatars/default-avatar-tech-guy.png"
       actAsExpander
     />
