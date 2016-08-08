a445571831e2b908aeb9509aa18c984d519f47d5
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-08-08 23:31:03 +0300

    Field validation working

--- app/components/authentication/signUp/SignUpForm.jsx
+++ app/components/authentication/signUp/SignUpForm.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
-import { TextField } from 'redux-form-material-ui';
+import TextField from 'material-ui/TextField';
 import Snackbar from 'material-ui/Snackbar';
 import request from 'superagent';
-
 
 const divStyle = {
   marginTop: '20px',
@@ -30,7 +29,7 @@ class SignUpForm extends React.Component {
 
     this.state = {
       snackBarOpen: false,
-      username: '#',
+      username: '',
       firstName: '',
       lastName: '',
       email: '',
@@ -38,32 +37,6 @@ class SignUpForm extends React.Component {
       formReady: false,
       errorText: ''
     };
-
-    this.signUpUser = this.signUpUser.bind(this);
-    this.validate = this.validate.bind(this);
-    this.isReady = this.isReady.bind(this);
-    this.handleOnChange = this.handleOnChange.bind(this);
-  }
-
-  signUpUser() {
-    const values = this.state.form;
-    console.log(values);
-    request
-      .post('api/users')
-      .send(values)
-      .end((error) => {
-        if (error) {
-          this.setState({
-            snackBarMessage: 'Error creating account, try again.',
-            snackBarOpen: true
-          });
-        }
-
-        this.setState({
-          snackBarMessage: 'Account created, Log in to continue.',
-          snackBarOpen: true
-        });
-      });
   }
 
   isAlphanumeric(value) {
@@ -77,33 +50,13 @@ class SignUpForm extends React.Component {
       '' :
       'A valid email is required';
   }
-  isAlpha(value) {
-    return /^[a-zA-Z0-9_]/.test(value);
-  }
-
-  isEm(value) {
-    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value);
-  }
-  isReady() {
-    const ready =
-      this.isAlpha(this.state.username) &&
-      this.isAlpha(this.state.firstName) &&
-      this.isAlpha(this.state.laastName) &&
-      this.isEm(this.state.email) &&
-      this.state.password.length > 0;
-    this.setState({ formReady: ready });
-    console.log('################');
-    console.log(this.state.username);
-    console.log(this.isAlpha(this.state.username));
-    console.log('################');
-  }
 
   validate(field) {
     switch (field) {
       case ('username'):
-        return this.isAlpha(this.state.username) ?
-          this.setState({ errorText: '' }) :
-          this.setState({ errorText: 'Invalid Value' });
+        return this.state.username ?
+          this.isAlphanumeric(this.state.username) :
+          'This field is required';
       case ('firstName'):
         return this.state.firstName ?
           this.isAlphanumeric(this.state.firstName) :
@@ -121,16 +74,8 @@ class SignUpForm extends React.Component {
           '' :
           'This field is required';
       default:
-        return 'abcd';
+        return '';
     }
-  }
-
-  handleOnChange(event) {
-    console.log('{{{{{{{{{{{{}}}}}}}}}}}}');
-    console.log(event.target.value);
-    console.log('{{{{{{{{{{{{}}}}}}}}}}}}');
-    this.setState({ username: event.target.value });
-    console.log(this.state);
   }
 
   render() {
@@ -141,10 +86,12 @@ class SignUpForm extends React.Component {
             style={{ width: '100%' }}
             hintText="Username"
             floatingLabelText="Username"
-            errorText={this.state.errorText}
             value={this.state.username}
+            errorText={this.validate('username')}
             onChange={
-                this.handleOnChange
+              (event) => {
+                this.setState({ username: event.target.value });
+              }
             }
           />
         </div>
@@ -157,7 +104,6 @@ class SignUpForm extends React.Component {
             onChange={
               (event) => {
                 this.setState({ firstName: event.target.value });
-                this.isReady();
               }
             }
           />
@@ -169,7 +115,6 @@ class SignUpForm extends React.Component {
             onChange={
               (event) => {
                 this.setState({ lastName: event.target.value });
-                this.isReady();
               }
             }
           />
@@ -183,7 +128,6 @@ class SignUpForm extends React.Component {
             onChange={
               (event) => {
                 this.setState({ email: event.target.value });
-                this.isReady();
               }
             }
           />
@@ -198,7 +142,6 @@ class SignUpForm extends React.Component {
             onChange={
               (event) => {
                 this.setState({ password: event.target.value });
-                this.isReady();
               }
             }
           />
@@ -207,25 +150,6 @@ class SignUpForm extends React.Component {
           style={buttonStyle}
           label="Sign Up"
           disabled={!this.state.formReady}
-          onClick={() => {
-            const values = this.state.form;
-            request
-              .post('api/users')
-              .send(values)
-              .end((error) => {
-                if (error) {
-                  this.setState({
-                    snackBarMessage: 'Error creating account, try again.',
-                    snackBarOpen: true
-                  });
-                }
-
-                this.setState({
-                  snackBarMessage: 'Account created, Log in to continue.',
-                  snackBarOpen: true
-                });
-              });
-          }}
         />
         <Snackbar
           open={this.state.snackBarOpen}
