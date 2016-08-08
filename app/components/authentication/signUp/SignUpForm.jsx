955386574a234739947bfdded311e2d0f9c52245
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-08-08 22:18:00 +0300

    Buggy but useful

--- app/components/authentication/signUp/SignUpForm.jsx
+++ app/components/authentication/signUp/SignUpForm.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
-import { Field, reduxForm } from 'redux-form';
 import { TextField } from 'redux-form-material-ui';
+import Snackbar from 'material-ui/Snackbar';
 import request from 'superagent';
 
-import store from '../../../redux/store';
-
-const fields = [
-  'username',
-  'email',
-  'firstName',
-  'lastName',
-  'password'
-];
 
 const divStyle = {
   marginTop: '20px',
@@ -33,113 +24,219 @@ const buttonStyle = {
   marginTop: '20px',
 };
 
-const validate = (values) => {
-  const errors = {};
-  const requiredFields = [
-    'firstName',
-    'lastName',
-    'password',
-    'username',
-    'email'
-  ];
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-  requiredFields.forEach((field) => {
-    if (!values[field]) {
-      errors[field] = 'Required';
-    }
-  });
+    this.state = {
+      snackBarOpen: false,
+      username: '#',
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      formReady: false,
+      errorText: ''
+    };
 
-  if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
+    this.signUpUser = this.signUpUser.bind(this);
+    this.validate = this.validate.bind(this);
+    this.isReady = this.isReady.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
   }
 
-  return errors;
-};
+  signUpUser() {
+    const values = this.state.form;
+    console.log(values);
+    request
+      .post('api/users')
+      .send(values)
+      .end((error) => {
+        if (error) {
+          this.setState({
+            snackBarMessage: 'Error creating account, try again.',
+            snackBarOpen: true
+          });
+        }
 
-const SignUpForm = (props) => {
-  return (
-    <div style={divStyle}>
-      <div style={fieldStyle}>
-        <Field
-          style={{ width: '100%' }}
-          name="username"
-          component={TextField}
-          hintText="Username"
-          floatingLabelText="Username"
-        />
-      </div>
-      <div style={fieldStyle}>
-        <Field
-          style={{ width: '49%', float: 'left' }}
-          name="firstName"
-          component={TextField}
-          hintText="First Name"
-          floatingLabelText="First Name"
-        />
-        <Field
-          style={{ width: '49%', float: 'right' }}
-          name="lastName"
-          component={TextField}
-          hintText="Last Name"
-          floatingLabelText="Last Name"
-        />
-      </div>
-      <div style={fieldStyle}>
-        <Field
-          style={{ width: '100%' }}
-          name="email"
-          component={TextField}
-          hintText="Email"
-          floatingLabelText="Email"
-        />
-      </div>
-      <div style={fieldStyle}>
-        <Field
-          style={{ width: '100%' }}
-          name="password"
-          component={TextField}
-          hintText="Password"
-          floatingLabelText="Password"
-          type="password"
-        />
-      </div>
-      <FlatButton
-        style={buttonStyle}
-        label="Sign Up"
-        onClick={props.handleSubmit}
-      />
-    </div>
-  );
-};
-
-SignUpForm.propTypes = {
-  handleSubmit: React.PropTypes.func.isRequired,
-};
-
-const signUpUser = (values) => {
-  console.log(values);
-  request
-    .post('api/users')
-    .send(values)
-    .end((error, response) => {
-      if (error) {
-        store.dispatch({
-          type: 'SIGN_UP_USER_FAILURE',
-          payload: {
-            error
-          }
+        this.setState({
+          snackBarMessage: 'Account created, Log in to continue.',
+          snackBarOpen: true
         });
-      }
-      console.log(response.body);
-    });
-};
-
-export default reduxForm({
-  form: 'simple',
-  validate,
-  fields,
-  onSubmit: (values) => {
-    signUpUser(values);
+      });
   }
-})(SignUpForm);
+
+  isAlphanumeric(value) {
+    return /^[a-zA-Z0-9_]/.test(value) ?
+      '' :
+      'This value must be alphanumeric';
+  }
+
+  isEmail(value) {
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+      '' :
+      'A valid email is required';
+  }
+  isAlpha(value) {
+    return /^[a-zA-Z0-9_]/.test(value);
+  }
+
+  isEm(value) {
+    return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value);
+  }
+  isReady() {
+    const ready =
+      this.isAlpha(this.state.username) &&
+      this.isAlpha(this.state.firstName) &&
+      this.isAlpha(this.state.laastName) &&
+      this.isEm(this.state.email) &&
+      this.state.password.length > 0;
+    this.setState({ formReady: ready });
+    console.log('################');
+    console.log(this.state.username);
+    console.log(this.isAlpha(this.state.username));
+    console.log('################');
+  }
+
+  validate(field) {
+    switch (field) {
+      case ('username'):
+        return this.isAlpha(this.state.username) ?
+          this.setState({ errorText: '' }) :
+          this.setState({ errorText: 'Invalid Value' });
+      case ('firstName'):
+        return this.state.firstName ?
+          this.isAlphanumeric(this.state.firstName) :
+          'This field is required';
+      case ('lastName'):
+        return this.state.lastName ?
+          this.isAlphanumeric(this.state.lastName) :
+          'This field is required';
+      case ('email'):
+        return this.state.email ?
+          this.isEmail(this.state.email) :
+          'This field is required';
+      case ('password'):
+        return this.state.password ?
+          '' :
+          'This field is required';
+      default:
+        return 'abcd';
+    }
+  }
+
+  handleOnChange(event) {
+    console.log('{{{{{{{{{{{{}}}}}}}}}}}}');
+    console.log(event.target.value);
+    console.log('{{{{{{{{{{{{}}}}}}}}}}}}');
+    this.setState({ username: event.target.value });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div style={divStyle}>
+        <div style={fieldStyle}>
+          <TextField
+            style={{ width: '100%' }}
+            hintText="Username"
+            floatingLabelText="Username"
+            errorText={this.state.errorText}
+            value={this.state.username}
+            onChange={
+                this.handleOnChange
+            }
+          />
+        </div>
+        <div style={fieldStyle}>
+          <TextField
+            style={{ width: '49%', float: 'left' }}
+            hintText="First Name"
+            floatingLabelText="First Name"
+            errorText={this.validate('firstName')}
+            onChange={
+              (event) => {
+                this.setState({ firstName: event.target.value });
+                this.isReady();
+              }
+            }
+          />
+          <TextField
+            style={{ width: '49%', float: 'right' }}
+            hintText="Last Name"
+            floatingLabelText="Last Name"
+            errorText={this.validate('lastName')}
+            onChange={
+              (event) => {
+                this.setState({ lastName: event.target.value });
+                this.isReady();
+              }
+            }
+          />
+        </div>
+        <div style={fieldStyle}>
+          <TextField
+            style={{ width: '100%' }}
+            hintText="Email"
+            floatingLabelText="Email"
+            errorText={this.validate('email')}
+            onChange={
+              (event) => {
+                this.setState({ email: event.target.value });
+                this.isReady();
+              }
+            }
+          />
+        </div>
+        <div style={fieldStyle}>
+          <TextField
+            style={{ width: '100%' }}
+            hintText="Password"
+            floatingLabelText="Password"
+            type="password"
+            errorText={this.validate('password')}
+            onChange={
+              (event) => {
+                this.setState({ password: event.target.value });
+                this.isReady();
+              }
+            }
+          />
+        </div>
+        <FlatButton
+          style={buttonStyle}
+          label="Sign Up"
+          disabled={!this.state.formReady}
+          onClick={() => {
+            const values = this.state.form;
+            request
+              .post('api/users')
+              .send(values)
+              .end((error) => {
+                if (error) {
+                  this.setState({
+                    snackBarMessage: 'Error creating account, try again.',
+                    snackBarOpen: true
+                  });
+                }
+
+                this.setState({
+                  snackBarMessage: 'Account created, Log in to continue.',
+                  snackBarOpen: true
+                });
+              });
+          }}
+        />
+        <Snackbar
+          open={this.state.snackBarOpen}
+          style={{ textAlign: 'center' }}
+          message={this.state.snackBarMessage}
+          onRequestClose={() => { this.setState({ snackBarOpen: false }); }}
+        />
+      </div>
+    );
+  }
+}
+
+export default SignUpForm;
 
